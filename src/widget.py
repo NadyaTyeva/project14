efb49aa75e7e42daae746b78538500831cfd4e5a
Nadezhda Tueva <mamamozhetvseru@mail.ru>
2024-09-08 20:09:47 +0300

    the second function

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,7 @@
 from typing import Union
 
-def mask_account_card(card_or_account: Union[str, int]) -> str:
+def mask_account_card(card_or_account: Union[str]) -> str:
     ''' Функция обрабатывает информацию как о картах, так и о счетах. '''
-
     if "Счет" in card_or_account:
         return card_or_account[:4] + " **" + card_or_account[-4:]
     else:
@@ -10,6 +9,13 @@ def mask_account_card(card_or_account: Union[str, int]) -> str:
 
 card_or_account = input()
 print(mask_account_card(card_or_account))
+
+def get_date(date: Union[str]) -> str:
+    ''' Функция принимает строку с датой и возвращает в формате "ДД.ММ.ГГГГ"'''
+    return date[8:10] + "." + date [5:7] + "." + date [:4]
+
+date = input()
+print(get_date(date))
 
 
 
