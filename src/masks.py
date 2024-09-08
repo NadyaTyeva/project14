972908e1ef0e344f3b9150738234214cffed3b11
Nadezhda Tueva <mamamozhetvseru@mail.ru>
2024-09-08 21:32:04 +0300

    the final version

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,19 @@
 from typing import Union
 
 
-def get_mask_card_number(card_number: Union[str, int]) -> str:
+def get_mask_card_number(card_or_account: Union[str, int]) -> str:
     """Функция маскирует номер банковской карты."""
-    return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[12:]} "
+    return f"{str(card_or_account)[:4]} {str(card_or_account)[4:6]}** **** {str(card_or_account)[12:]} "
 
 
-card_number = input()
-print(get_mask_card_number(card_number))
+#card_number = input()
+#print(get_mask_card_number(card_number))
 
 
-def get_mask_account(account_number: Union[str, int]) -> str:
+def get_mask_account(card_or_account: Union[str, int]) -> str:
     """Функция маскирует номер банковского счета."""
-    return f"**{str(account_number)[-4:]}"
+    return f"**{str(card_or_account)[-4:]}"
 
 
-account_number = input()
-print(get_mask_account(account_number))
+#account_number = input()
+#print(get_mask_account(account_number))
--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,23 @@
 from typing import Union
 
+
 def mask_account_card(card_or_account: Union[str]) -> str:
     ''' Функция обрабатывает информацию как о картах, так и о счетах. '''
     if "Счет" in card_or_account:
-        return card_or_account[:4] + " **" + card_or_account[-4:]
-    else:
-        return card_or_account[:-12] + " " + card_or_account[-12:-10]+ "** **** " + card_or_account[-4:]
+        from masks import get_mask_account
+        return card_or_account[:4] + " " + get_mask_account(card_or_account[5:])
 
-card_or_account = input()
-print(mask_account_card(card_or_account))
+    else:
+        from masks import get_mask_card_number
+        return card_or_account[:-16] + " " + get_mask_card_number(card_or_account[-16:])
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+
 
 def get_date(date: Union[str]) -> str:
     ''' Функция принимает строку с датой и возвращает в формате "ДД.ММ.ГГГГ"'''
-    return date[8:10] + "." + date [5:7] + "." + date [:4]
+   return date[8:10] + "." + date [5:7] + "." + date [:4]
 
 date = input()
 print(get_date(date))
-
-
 
