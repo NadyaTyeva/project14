f8ebc0b84b640460d9bd01ebc20b6b0e54836c36
Nadezhda Tueva <mamamozhetvseru@mail.ru>
2024-09-16 20:09:37 +0300

    add test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,9 +3,15 @@ from typing import Union
 
 def get_mask_card_number(card_or_account: Union[str, int]) -> str:
     """Функция маскирует номер банковской карты."""
-    return f"{str(card_or_account)[:4]} {str(card_or_account)[4:6]}** **** {str(card_or_account)[12:]}"
+    if len(card_or_account) == 16:
+        return f"{str(card_or_account)[:4]} {str(card_or_account)[4:6]}** **** {str(card_or_account)[12:]}"
+    else:
+        return "Введите корректный номер карты"
 
 
-def get_mask_account(card_or_account: Union[str, int]) -> str:
+def get_mask_account(card_or_account_: Union[str, int]) -> str:
     """Функция маскирует номер банковского счета."""
-    return f"**{str(card_or_account)[-4:]}"
+    if len(card_or_account_) == 20:
+        return f"**{str(card_or_account_)[-4:]}"
+    else:
+        return "Введите корректный номер счета"
