20014c0e702e8cd8571b5bf7852a5f63653bcd61
Nadezhda Tueva <mamamozhetvseru@mail.ru>
2024-10-01 18:50:32 +0300

    with decorators+test

--- src/decorators.py
+++ src/decorators.py
@@ -3,36 +3,35 @@ from functools import wraps
 from typing import Any, Optional, Callable
 
 
-def log(filename: Optional[str]) -> Callable:
+def log(filename: Optional[str] = None) -> Callable:
+
     '''Автоматически логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки'''
+    
     def my_decorator(func: Any) -> Any:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{func.__name__} ok")
+                    with open(filename, "a", encoding = "utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
                 else:
                     print(f"{func.__name__} ok")
                 return result
             except Exception as e:
                 if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+                    with open(filename, "a", encoding = "utf-8") as file:
+                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}\n")
                 else:
                     print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
-                raise e
         return wrapper
     return my_decorator
 
 
-@log(filename='mylog.txt')
+@log()
 def my_function(x: int, y: int) -> int:
     '''складывает два значения'''
     return x + y
 
 
-my_function(1, 2)
-
-print(my_function(1, 2))
+#print(my_function(1, "2"))
