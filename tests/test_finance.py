ed3a109fd7f530d0e2d395b1e3c1df81d6019515
Nadezhda Tueva <mamamozhetvseru@mail.ru>
2024-10-13 14:03:15 +0300

    with test finance

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -2,7 +2,7 @@ import json
 import pytest
 import pandas as pd
 from unittest.mock import patch
-from src.finance import financial_transactions_csv
+from src.finance import financial_transactions_csv,transactions_from_excel
 
 
 @patch('src.finance.pd')
@@ -22,28 +22,19 @@ def test_financial_transactions_csv(mocked_pandas):
     ]
     mocked_pandas.read_csv.assert_called_once_with("transactions.csv")
 
-#
-#
-#
-# @patch('pd.read_csv')
-# def test_financial_transactions_csv(mock_get):
-#     mock_data = pd.DataFrame({
-#         'date': ['2023-01-01', '2023-01-02'],
-#         'amount': [100, 200],
-#         'description': ['Income', 'Expense']
-#     })
-#     mock_read_csv.return_value = mock_data
-#
-#     # Запуск тестируемой функции
-#     result = financial_transactions_csv("transactions.csv")
-#
-#     # результат, который ожидаем
-#     expected_result = [
-#         {'date': '2023-01-01', 'amount': 100, 'description': 'Income'},
-#         {'date': '2023-01-02', 'amount': 200, 'description': 'Expense'}
-#     ]
-#
-#     # Проверка что результат ожидаемы равен полученному
-#     self.assertEqual(result, expected_result)
-#     mock_read_csv.assert_called_once_with("transactions.csv")
+@patch('src.finance.pd')
+def test_transactions_from_excel(mocked_pandas):
 
+    mocked_pandas.read_excel.return_value = pd.DataFrame({
+        'date': ['2023-01-01', '2023-01-02'],
+        'amount': [100, 200],
+        'description': ['Payment A', 'Payment B']
+    })
+
+    result = transactions_from_excel("transactions_excel.xlsx")
+
+    assert result == [
+        {'date': '2023-01-01', 'amount': 100, 'description': 'Payment A'},
+        {'date': '2023-01-02', 'amount': 200, 'description': 'Payment B'}
+    ]
+    mocked_pandas.read_excel.assert_called_once_with("transactions_excel.xlsx")
